c5224361d0404cad5bce0b4ecc8879be7bfcb016
louixs <ryuei.sasaki@gmail.com>
2016-11-03 19:29:44 +0000

    cleaned up

--- tsushin.widget/tsushin.sh
+++ tsushin.widget/tsushin.sh
@@ -5,16 +5,11 @@ netstat -iw 1 | head -n3 | tail -n1 | awk '{print $3 " " $6}' > tsushin.db &
 
 process=$!
 
-#echo $process
-#echo "process PID: "$process
-#echo "obtained data. sleeping..."
 sleep 1.5
-#echo "killing last run PID: " $process
+
 pkill -P $process          
 
 in=$(cat tsushin.db | awk '{print $1}')
 out=$(cat tsushin.db | awk '{print $2}')
-#echo "In : $in MB"
-#echo "Out: $out MB"
 echo $in $out
-#echo "val1 val2"
+
