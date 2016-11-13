51aa986a6104fbdd99fefcac6432510b934d0b7e
louixs <ryuei.sasaki@gmail.com>
2016-11-13 11:34:14 +0000

    improved portability by adding conditions so to detect whether .bash_profile exissts in currently working directory. Now it should work to to just copy tsushin.widget folder in the main widghet directory

--- tsushin.widget/tsushin.sh
+++ tsushin.widget/tsushin.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
-source .bash_profile  &&
 
-netstat -iw 1 | head -n3 | tail -n1 | awk '{print $3 " " $6}' > tsushin.db &
+# Ubersicht widget : tsushin
+# By Ryuei Sasaki
+# 2016
+# https://github.com/louixs/tsushin
 
-process=$!
+#################
+#### READ ME ####
+#################
+# This script needs bash to work as opposed to default shell that ubersicht uses
+# To avoid running into error, I have included .bash_profile in the same directory and this scirpt is using it
+# This .bash_profile includes paths where most of the recent Mac OS X has bash by defualt
+# If you already have your .bash_profile please comment out source .bash_profile  && and uncomment the #source path_to_your_bash_profile && line to specify the path to your own .bash_profile and please make sure that your .bash_profile actually includes a path to bash
+# Happy coding
+################
 
-sleep 1.5
+echo $PWD/tsushin.widget
 
-pkill -P $process          
+function getThroughput(){
 
-in=$(cat tsushin.db | awk '{print $1}')
-out=$(cat tsushin.db | awk '{print $2}')
-echo $in $out
+  netstat -iw 1 | head -n3 | tail -n1 | awk '{print $3 " " $6}' > tsushin.db &
 
+  process=$!
+  sleep 1.5
+  pkill -P $process          
+  in=$(cat tsushin.db | awk '{print $1}')
+  out=$(cat tsushin.db | awk '{print $2}')
+
+  echo $in $out
+}
+
+
+#source path_to_your_bash_profile &&
+if [ -e $PWD/.bash_profile ]; then
+  source $PWD/.bash_profile &&
+  getThroughput
+else
+  source $PWD/tsushin.widget/.bash_profile  &&
+  getThroughput
+fi
